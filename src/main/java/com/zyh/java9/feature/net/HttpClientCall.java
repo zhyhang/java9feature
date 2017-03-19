396aa8552515c52da41eef19f5435c4fbcd553a6
zhyhang <zhyhang1978@gmail.com>
2017-03-19 22:53:24 +0800

    add jdk httpclient test

---
+++ src/main/java/com/zyh/java9/feature/net/HttpClientCall.java
@@ -0,0 +1,13 @@
+package com.zyh.java9.feature.net;
+
+import jdk.incubator.http.HttpClient;
+import jdk.incubator.http.HttpRequest;
+
+/**
+ * Created by zhyhang on 2017/3/19.
+ */
+public class HttpClientCall {
+    public static void main(String... args) {
+        HttpClient client = HttpClient.newHttpClient();
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,5 +3,5 @@
  */
 module com.zyh.java9.trying {
     requires jdk.unsupported;
-//    requires java.base;
+    requires jdk.incubator.httpclient;
 }
