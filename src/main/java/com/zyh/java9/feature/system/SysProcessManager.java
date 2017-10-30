ed82dbb20007deadc51a45ac50033191f4ff57f7
zhyhang <zhyhang1978@gmail.com>
2017-10-30 22:36:53 +0800

    fix process api with GA java9

--- src/main/java/com/zyh/java9/feature/system/SysProcessManager.java
+++ src/main/java/com/zyh/java9/feature/system/SysProcessManager.java
@@ -11,13 +11,13 @@ public class SysProcessManager {
         // get current process i.e. this jvm process
         ProcessHandle currentProcess = ProcessHandle.current();
         ProcessHandle.Info info = currentProcess.info();
-        System.out.format("current process OS id is %d.\n", currentProcess.getPid());
+        System.out.format("current process OS id is %d.\n", currentProcess.pid());
         System.out.format("\tcommand line %s\n", info.commandLine().orElse(""));
         System.out.format("\tprogram name %s\n", info.command().orElse(""));
         System.out.format("\tprogram arguments %s\n", Arrays.deepToString(info.arguments().orElse(new String[0])));
         System.out.format("\towner %s\n", info.user().orElse(""));
         // get all process
         System.out.println("current run processes:");
-        ProcessHandle.allProcesses().forEach(p -> System.out.format("%s\t%d\t%s\n", p.info().user().orElse(""), p.getPid(), p.info().command().orElse("")));
+        ProcessHandle.allProcesses().forEach(p -> System.out.format("%s\t%d\t%s\n", p.info().user().orElse(""), p.pid(), p.info().command().orElse("")));
     }
 }
